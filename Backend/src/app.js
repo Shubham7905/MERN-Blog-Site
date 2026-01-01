5e508223205c68497be5e91464d8a3812e0fccaa
Lenovo <singhshubham11122022@gmail.com>
2026-01-01 11:18:00 +0530

    added dist

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,16 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: true,
     credentials: true
 }))
 app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
-app.use(express.static("dist"))
 app.use(cookieParser())
 
 //routes
@@ -19,5 +19,12 @@ import blogRouter from "./routes/blog.routes.js"
 
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/blogs", blogRouter)
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 export { app }
