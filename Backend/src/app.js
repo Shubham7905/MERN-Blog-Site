cfbdb1b3979b371c8ce3dbde5c5e65abd0147ed5
Lenovo <singhshubham11122022@gmail.com>
2026-01-01 09:15:02 +0530

    Fix issues

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,8 +12,8 @@ app.use(cors({
 }))
 app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
-app.use(express.static("public"))
 app.use(cookieParser())
+app.use(express.static("public"))
 
 //routes
 import userRouter from "./routes/user.routes.js"
--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -148,7 +148,7 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 });
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
-    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+    const incomingRefreshToken = req.cookies?.refreshToken || req.body?.refreshToken
 
     if (!incomingRefreshToken) {
         throw new ApiError(401, "Unauthorized request")
